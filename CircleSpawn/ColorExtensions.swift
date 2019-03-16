059eaef64f1b07a289ccbf941773aa889f42da0e
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2019-03-16 12:39:53 +0100

    Add color extensions

---
+++ CircleSpawn/ColorExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  ColorExtensions.swift
+//  CircleSpawn
+//
+//  Created by Bartosz Selwesiuk on 16/03/2019.
+//  Copyright © 2019 Bartosz Selwesiuk. All rights reserved.
+//
+
+import UIKit
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return random(min: 0.0, max: 1.0)
+    }
+    
+    static func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        assert(max > min)
+        return min + ((max - min) * CGFloat(arc4random()) / CGFloat(UInt32.max))
+    }
+}
+
+extension UIColor {
+    static func randomBrightColor() -> UIColor {
+        return UIColor(hue: .random(),
+                       saturation: .random(min: 0.5, max: 1.0),
+                       brightness: .random(min: 0.7, max: 1.0),
+                       alpha: 1.0)
+    }
+}
