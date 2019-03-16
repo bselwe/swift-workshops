9da8323f59a9e194bddbd6c763495788da9acb9d
Bartosz Selwesiuk <bselwesiuk@gmail.com>
2019-03-16 12:41:45 +0100

    Add view controller logic

--- CircleSpawn/ViewController.swift
+++ CircleSpawn/ViewController.swift
@@ -8,10 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    private var addGesture: UITapGestureRecognizer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initializeGestures()
+    }
+    
+    private func initializeGestures() {
+        addGesture = UITapGestureRecognizer(target: self, action: #selector(handleAdd(_:)))
+        addGesture!.numberOfTapsRequired = 2
+        view.addGestureRecognizer(addGesture!)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return otherGestureRecognizer == addGesture
+    }
+    
+    @objc func handleAdd(_ tap: UITapGestureRecognizer) {
+        let spawnedView = CircleView(center: tap.location(in: view))
+        view.addSubview(spawnedView)
+        
+        let removeGesture = UITapGestureRecognizer(target: self, action: #selector(handleRemove(_:)))
+        removeGesture.numberOfTapsRequired = 3
+        removeGesture.delegate = self
+        
+        let moveGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleMove(_:)))
+        
+        spawnedView.addGestureRecognizer(removeGesture)
+        spawnedView.addGestureRecognizer(moveGesture)
+        
+        spawnedView.showWithAnimation()
+    }
+    
+    @objc func handleRemove(_ tap: UITapGestureRecognizer) {
+        let spawnedView = tap.view as! CircleView
+        spawnedView.removeWithAnimation()
+    }
+    
+    @objc func handleMove(_ tap: UILongPressGestureRecognizer) {
+        let spawnedView = tap.view as! CircleView
+        
+        switch tap.state {
+        case .began:
+            spawnedView.animateZoomIn(tap)
+        case .cancelled, .ended, .failed:
+            spawnedView.animateZoomOut()
+        case .changed:
+            spawnedView.move(tap.location(in: spawnedView))
+        default:
+            break
+        }
     }
 }
 
